66da4a278da61c209403fcf7361effd96f49a6e5
sinhakushagra123 <kushagrasinha3596@gmail.com>
2019-05-14 07:24:08 +0530

    using express to handle post data

--- main.js
+++ main.js
@@ -1,30 +1,41 @@
 var res = module.require("./DatabaseConnectivity");
-function getURL(){
-var url = document.getElementById("urlinput").value;
-var protocol = url.startsWith("https://") || url.startsWith("http://");
-if(!protocol){
-    url="http://"+url;
-    return url;
- }
- return url;
-} 
+var express = require("express");
+var bodyParser = require('body-parser');
+var app = express();
+var urlEncodedParser = app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+  }));
+app.get('/generateShortURL',urlEncodedParser, function(req,res){
+console.log("Kushagra "+req.body);
+});
 
-function generateShortURL(){
-    //var longurl = getURL();
-    var longurl = "www.w3schools.com";
-    var shorturl = longurl.toString('base64').substring(0,7);
+// function getURL(){
+// var url = document.getElementById("urlinput").value;
+// var protocol = url.startsWith("https://") || url.startsWith("http://");
+// if(!protocol){
+//     url="http://"+url;
+//     return url;
+//  }
+//  return url;
+// } 
 
-    //establishing database connection
-    // res.connection.connect();
+// function generateShortURL(){
+//     //var longurl = getURL();
+//     var longurl = "www.w3schools.com";
+//     var shorturl = longurl.toString('base64').substring(0,7);
 
-    //check whether the short url exist for the given long url or not
-     res.checkURL(longurl).then(function(data){
-         console.log("Short URL already exists and it is: "+data);
-     },function(message){
-         res.insertShortURL(longurl,shorturl)
-         .then(function(success){console.log("Short URL inserted successfuly");},
-              function(failure){console.log("Short URL insertion unsuccessful");});
-     });
-    }
+//     //establishing database connection
+//     // res.connection.connect();
 
- generateShortURL();
+//     //check whether the short url exist for the given long url or not
+//      res.checkURL(longurl).then(function(data){
+//          console.log("Short URL already exists and it is: "+data);
+//      },function(message){
+//          res.insertShortURL(longurl,shorturl)
+//          .then(function(success){console.log("Short URL inserted successfuly");},
+//               function(failure){console.log("Short URL insertion unsuccessful");});
+//      });
+//     }
+
+//  generateShortURL();
+app.listen(3000);
--- test.js
+++ test.js
@@ -1,6 +1,12 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(req.name);
-    res.end("Hello Kushagra");
-  }).listen(8080);
+var express = require("express");
+var bodyParser = require('body-parser');
+var app = express();
+var urlEncodedParser = app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: false
+}));
+app.post('/test',urlEncodedParser, function(req,res){
+console.log("Kushagra "+req.body);
+});
+app.listen(8080,function(){
+  console.log("Started in port 8080");
+});
